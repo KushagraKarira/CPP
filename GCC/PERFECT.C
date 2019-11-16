310a10bcfb5d7fa42a76f8b910a053ef1c836bec
kushagrakarira <kushagrakarira@gmail.com>
2019-11-16 10:21:26 +0530

    Update

---
+++ GCC/PERFECT.C
@@ -0,0 +1,16 @@
+#include<stdio>
+
+int main()
+{
+  int first, last, rem, sum = 0, i;
+  printf("Enter first number : ");
+  scanf("%d", first );
+
+  printf("Enter last number : ");
+  scanf("%d", last );
+
+  return 0;
+}
+
+//How it works :
+//Takes in first, checks if it is perfect, prints if true, else first++ ( till first == last )
